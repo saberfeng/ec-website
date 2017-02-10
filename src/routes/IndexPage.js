d1fd571cd127c96857770bfb68794218b357947e
saberfeng <saberfeng@outlook.com>
2017-02-10 23:28:11 +0800

    carousel problem fixed

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -5,69 +5,32 @@ import HeadBar from '../components/HeadBar';
 import ItemList from '../components/ItemList';
 import FootBar from '../components/FootBar';
 import { Carousel } from 'antd';
-import Slider from 'react-slick';
-
-
-function CustomSlides({slide}) {
-  return (<img className={styles.carousel} src={slide.photoSrc} />);
-}
 
 function IndexPage({items, carousel}) {
-  /*
-  const images = carousel.map((image, i) => {
-    return <div><img className={styles.carousel} src={image.photoSrc} /></div>
-  });
-  */
+
   const slides = carousel.map(slide => {
-    return <div><CustomSlides slide={slide} /></div>
+    return <div key={slide.id}><img className={styles.carousel} src={slide.photoSrc} /></div>
   });
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  }
+
   return (
     <div className={styles.normal}>
-      {/*
-      <Carousel autoplay>
-        {images}
-      </Carousel>
-
-    <Slider>
-      { carousel.map(slide => <div><CustomSlides /></div>) }
-    </Slider> 
-      */
-      }
-
       {slides.length ? <Carousel autoplay>{slides}</Carousel> : null}
-
+      <div className={styles.product_title}>
+        <span className={styles.rxdp}>热销单品</span>
+      </div>
       <HeadBar />
       <ItemList items={items} />
       <FootBar />
     </div>
   );
 }
-/*
-IndexPage.propTypes = {
-};
-*/
+
 function mapStateToProps(state) {
   return {
     items: state.app.itemsById,
     carousel: state.app.carousel
   }
 }
-/**
- * <div className={styles.product_title}>
-        <span className={styles.rxdp}>热销单品</span>
-      </div>
- */
 
-
-/*
-{items.map((item,i)=>{<Item item={item} key={item.id}/>})}
-*/
 
 export default connect(mapStateToProps)(IndexPage);
